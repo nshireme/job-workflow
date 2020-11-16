c846960cde15de5da0581c3589bd5d4da3ad1638
ns.hireme <ns.hireme@gmail.com>
2020-11-16 14:48:27 -0500

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+
+set automate_workflow=%USERPROFILE%\Documents\Work\automate-workflow.tcl
+set job_file="job.txt"
+
+echo Running %automate_workflow%
+tclsh "%automate_workflow%"
+pause
+
+set /p file_path=<"%job_file%"
+echo Starting %file_path%
+start /wait "" "%file_path%"
+del /p "%job_file%"
+
+exit /b
